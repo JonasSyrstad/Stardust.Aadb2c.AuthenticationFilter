fe69ce39e1d68ebe5da0a1c2814e0306fd598b46
Jonas Syrstad <jonas.syrstad@dnvgl.com>
2017-08-15 09:39:39 +0200

    added additional parameters to swagger ui

--- src/Stardust.Aadb2c.Swagger/SwaggerUiOAuthExtensions.cs
+++ src/Stardust.Aadb2c.Swagger/SwaggerUiOAuthExtensions.cs
@@ -8,18 +8,24 @@ namespace Stardust.Aadb2c.Swagger
 {
     public static class SwaggerUiOAuthExtensions
     {
-        public static void EnableAzureAdB2cOAuth2(this SwaggerUiConfig c, string clientId, string policyName, string clientSecret = null, string realm = null, string appName = "Swagger UI", bool enableDiscoveryUrlSelector = true)
+        public static void EnableAzureAdB2cOAuth2(this SwaggerUiConfig c, string clientId, string policyName, string clientSecret = null, string realm = null, string appName = "Swagger UI", bool enableDiscoveryUrlSelector = true, IDictionary<string, string> additionalParameters = null)
         {
+            var additionalParams = new Dictionary<string, string>
+            {
+                {"p", policyName},
+                {"response_mode", "fragment"}
+            };
+            if (additionalParameters != null)
+                foreach (var additionalParameter in additionalParameters)
+                {
+                    additionalParams.Add(additionalParameter.Key, additionalParameter.Value);
+                }
             c.EnableOAuth2Support(
                 clientId,
                 clientSecret,
                 realm,
                 appName,
-                additionalQueryStringParams: new Dictionary<string, string>
-                {
-                    {"p", policyName},
-                    {"response_mode", "fragment"}
-                }
+                additionalQueryStringParams: additionalParams
             );
             if (enableDiscoveryUrlSelector)
                 c.EnableDiscoveryUrlSelector();
@@ -27,7 +33,12 @@ namespace Stardust.Aadb2c.Swagger
 
         public static void EnableAzureAdB2cOAuth2(this SwaggerUiConfig c)
         {
-            c.EnableAzureAdB2cOAuth2(B2cGlobalSwaggerConfig.ClientId,B2cGlobalSwaggerConfig.Policy,B2cGlobalSwaggerConfig.AppName,B2cGlobalSwaggerConfig.ClientSecret);
+            c.EnableAzureAdB2cOAuth2(B2cGlobalSwaggerConfig.ClientId, B2cGlobalSwaggerConfig.Policy, B2cGlobalSwaggerConfig.AppName, B2cGlobalSwaggerConfig.ClientSecret);
+        }
+
+        public static void EnableAzureAdB2cOAuth2(this SwaggerUiConfig c, IDictionary<string, string> additionalParameters)
+        {
+            c.EnableAzureAdB2cOAuth2(B2cGlobalSwaggerConfig.ClientId, B2cGlobalSwaggerConfig.Policy, B2cGlobalSwaggerConfig.AppName, B2cGlobalSwaggerConfig.ClientSecret, additionalParameters: additionalParameters);
         }
     }
 }
