b832716948df63a8d1a7f0c97492e89ac85968d6
Jonas Syrstad <jonas.syrstad@dnvgl.com>
2019-07-03 15:25:09 +0200

    ups... deleted too many files...

--- src/netCore/Stardust.Aadb2c.AuthenticationFilterNetCore/Core/B2COAuthHandler.cs
+++ src/netCore/Stardust.Aadb2c.AuthenticationFilterNetCore/Core/B2COAuthHandler.cs
@@ -3,12 +3,40 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Stardust.Aadb2c.AuthenticationFilter.Core;
 using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Stardust.Particles;
 
 namespace Stardust.Aadb2c.AuthenticationFilter
 {
+    public class CoreConfigurationManager : IConfigurationReader
+    {
+        private NameValueCollection _collection;
+
+        public CoreConfigurationManager(IConfigurationBuilder builder)
+        {
+            _collection = new NameValueCollection();
+            foreach (var builderProperty in builder.Properties)
+            {
+                _collection.Add(builderProperty.Key, builderProperty.Value.ToString());
+
+            }
+        }
+
+        public CoreConfigurationManager(IDictionary<string, string> configSettings)
+        {
+            foreach (var builderProperty in configSettings)
+            {
+                _collection.Add(builderProperty.Key, builderProperty.Value);
+            }
+        }
+
+        public NameValueCollection AppSettings => _collection;
+    }
     public class B2COAuthHandler : RemoteAuthenticationHandler<B2COptions>
     {
         private readonly IServiceProvider _provider;
